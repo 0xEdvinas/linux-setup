e3fc80348397658634804405a8164569d58b58ee
Edvinas Bureika <edvinasbureika@gmail.com>
2026-03-24 19:33:17 +0200

    docker

--- setup.sh
+++ setup.sh
@@ -69,6 +69,32 @@ flatpak install flathub com.google.Chrome org.mozilla.firefox com.bitwarden.desk
 # DEV
 #
 
+# Remove old docker packages
+sudo dnf remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-engine -y
+
+# Required tools
+sudo dnf -y install dnf-plugins-core -y
+
+# Docker official repo
+sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+
+# Install docker engine
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Start and enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Run docker without sudo
+sudo usermod -aG docker $USER
+
 flatpak install flathub com.visualstudio.code com.google.AndroidStudio com.jetbrains.PyCharm-Professional com.jetbrains.IntelliJ-IDEA-Community com.jetbrains.IntelliJ-IDEA-Ultimate com.jetbrains.WebStorm com.jetbrains.CLion com.jetbrains.Rider com.jetbrains.DataGrip com.jetbrains.PhpStorm com.jetbrains.RustRover com.jetbrains.GoLand cc.arduino.IDE2 io.dbeaver.DBeaverCommunity -y
 
 # Py
